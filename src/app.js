422cb1665eee6f9e1d8773f35b18d59fcb966837
EssieQ <ecauven@gmail.com>
2021-06-09 18:27:23 +1000

    added weather icon
    It shows the changing weather now

--- src/app.js
+++ src/app.js
@@ -40,6 +40,12 @@ function showWeather(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function searchCity(city) {
